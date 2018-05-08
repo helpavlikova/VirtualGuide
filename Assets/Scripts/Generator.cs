fe5ce5356c0ff07d380eca6659ec3de6c23f2fb3
Helena <helpavlikova@gmail.com>
2018-05-08 19:16:11 +0200

    pavement and better hosue scale

--- Assets/Scripts/Generator.cs
+++ Assets/Scripts/Generator.cs
@@ -5,7 +5,8 @@ using UnityEngine;
 public class Generator : MonoBehaviour {
 
     public Transform example;
-    Transform buildings;
+    GameObject buildings;
+    Transform buildingsTrans;
     DataReader data = new DataReader();
 
 
@@ -14,14 +15,18 @@ public class Generator : MonoBehaviour {
         data.LoadGameData();
 
         //a parent object for all of the buildings
-        buildings = new GameObject("Buildings").transform;
+        buildings = new GameObject("Buildings");
+        buildingsTrans = new GameObject("Buildings").transform;
 
         for (int i = 0; i < data.GetSize(); i++) {
             Transform newObject = Instantiate(example, example.position, example.rotation);
             newObject.GetComponent<Building>().Set(data.GetModel(i));
-            newObject.SetParent(buildings);
+            newObject.SetParent(buildingsTrans);
         }
-	}
+
+        Rigidbody gameObjectsRigidBody = buildings.AddComponent<Rigidbody>(); // Add the rigidbody.
+        gameObjectsRigidBody.mass = 5; // Set the GO's mass to 5 via the Rigidbody.
+    }
 	
 	// Update is called once per frame
 	void Update () {
